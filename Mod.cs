e79602d598279869fcb355f630fafbbd96f0c9b1
Marcellino Abdelmalak <marcellinoabdelmalak@gmail.com>
2023-07-28 11:32:59 -0700

    fixed Sugar Cane & added mod to Workshop

--- Mod.cs
+++ Mod.cs
@@ -12,7 +12,7 @@ namespace GoldenFoodsNS
             WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.CookingIdea, "goldenfoods_blueprint_golden_berry", 1);
             WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.CookingIdea, "goldenfoods_blueprint_golden_apple", 1);
             WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.CookingIdea, "goldenfoods_blueprint_golden_banana", 1);
-            WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.CookingIdea, "goldenfoods_blueprint_golden_cane_sugar", 1);
+            WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.CookingIdea, "goldenfoods_blueprint_golden_sugar_cane", 1);
             WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.CookingIdea, "goldenfoods_blueprint_golden_bottle_of_rum", 1);
             WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.CookingIdea, "goldenfoods_blueprint_golden_carrot", 1);
             WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.CookingIdea, "goldenfoods_blueprint_golden_ceviche", 1);
